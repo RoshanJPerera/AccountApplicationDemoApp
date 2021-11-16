4547bece1a65d12b03979483ff14c04d46ffd9d1
P. G. J. Perera <42792184+RoshanJPerera@users.noreply.github.com>
2021-11-17 00:41:40 +0530

    Add files via upload

---
+++ AccountApplicationDemoApp/AccountForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace AccountApplicationDemoApp
+{
+    partial class AccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtBalance = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDestroy = new System.Windows.Forms.Button();
+            this.btnGC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Balance";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(30, 52);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(83, 22);
+            this.txtId.TabIndex = 1;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(119, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(83, 22);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtBalance
+            // 
+            this.txtBalance.Location = new System.Drawing.Point(208, 52);
+            this.txtBalance.Name = "txtBalance";
+            this.txtBalance.Size = new System.Drawing.Size(83, 22);
+            this.txtBalance.TabIndex = 5;
+            this.txtBalance.TextChanged += new System.EventHandler(this.txtBalance_TextChanged);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(30, 96);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(119, 96);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 7;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(208, 96);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 8;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(30, 125);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDestroy
+            // 
+            this.btnDestroy.Location = new System.Drawing.Point(119, 125);
+            this.btnDestroy.Name = "btnDestroy";
+            this.btnDestroy.Size = new System.Drawing.Size(75, 23);
+            this.btnDestroy.TabIndex = 10;
+            this.btnDestroy.Text = "Destroy";
+            this.btnDestroy.UseVisualStyleBackColor = true;
+            this.btnDestroy.Click += new System.EventHandler(this.btnDestroy_Click);
+            // 
+            // btnGC
+            // 
+            this.btnGC.Location = new System.Drawing.Point(208, 125);
+            this.btnGC.Name = "btnGC";
+            this.btnGC.Size = new System.Drawing.Size(75, 23);
+            this.btnGC.TabIndex = 11;
+            this.btnGC.Text = "GC";
+            this.btnGC.UseVisualStyleBackColor = true;
+            this.btnGC.Click += new System.EventHandler(this.btnGC_Click);
+            // 
+            // AccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 200);
+            this.Controls.Add(this.btnGC);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnDestroy);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtBalance);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AccountForm";
+            this.Text = "Account Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtBalance;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDestroy;
+        private System.Windows.Forms.Button btnGC;
+    }
+}
+
---
+++ AccountApplicationDemoApp/AccountForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountApplicationDemoApp
+{
+    public partial class AccountForm : Form
+    {
+        public AccountForm()
+        {
+            InitializeComponent();
+        }
+        Account a;
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBalance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            a = new Account();
+        }
+        
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            a.Id = int.Parse(txtId.Text);
+            a.Name = txtName.Text;
+            a.Balance = decimal.Parse(txtBalance.Text);
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            txtId.Text = a.Id.ToString();
+            txtName.Text = a.Name;
+            txtBalance.Text = a.Balance.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtBalance.Text = "";
+        }
+
+        private void btnDestroy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGC_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
